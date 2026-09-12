5719fe6f9e1396998f1461265ed6162e671642c0
Ananya Sriniketh <asriniketh29@cmc.edu>
2026-09-12 16:53:10 -0700

    5 passed tests in simple.py

--- src/simple.py
+++ src/simple.py
@@ -20,7 +20,7 @@ def evens(n):
     >>> evens(-1)
     []
     '''
-    evens = list(filter(lambda x: x % 2 == 0, n))
+    evens = list(filter(lambda x: x % 2 == 0, range(n+1)))
     return evens
 
 def threes(n):
@@ -38,7 +38,7 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
-
+    return list(filter(lambda x: '3' in str(x), range(n+1)))
 
 def small_words(text):
     '''
@@ -56,7 +56,7 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
-
+    return list(filter(lambda x: len(x) < 5, text.split()))
 
 def squares(n):
     '''
@@ -72,7 +72,7 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
-
+    return list(map(lambda x: x**2, range(1, n+1)))
 
 def lengths(strings):
     '''
@@ -85,3 +85,4 @@ def lengths(strings):
     >>> lengths(['this','is','a','test'])
     [4, 2, 1, 4]
     '''
+    return list(map(len, strings))
